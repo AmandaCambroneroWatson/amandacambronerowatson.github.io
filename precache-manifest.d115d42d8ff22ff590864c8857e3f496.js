20bbd6540361d4a19e4fdfe4ea71f62f9f4796f0
Amanda C.Watson <amandacwatson88@gmail.com>
2021-02-04 12:40:38 +0100

    Add first version

---
+++ precache-manifest.d115d42d8ff22ff590864c8857e3f496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f555871e87c0e8698357",
+    "url": "/css/app.d861a749.css"
+  },
+  {
+    "revision": "26aaf3de0b340fd8de1b",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "74c9d953d01e380c45659fa6b113ee5a",
+    "url": "/img/msa.logo.74c9d953.png"
+  },
+  {
+    "revision": "c8a113bee2a190664929895da9f5295a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f555871e87c0e8698357",
+    "url": "/js/app.6a64e722.js"
+  },
+  {
+    "revision": "26aaf3de0b340fd8de1b",
+    "url": "/js/chunk-vendors.8b606edf.js"
+  },
+  {
+    "revision": "8155d36ac5d8ed548ecab6f6c951ef99",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
